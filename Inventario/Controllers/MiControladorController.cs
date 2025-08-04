4fce7ee8dcf831d77cb64bd1a7ae11ce3b470a42
ElGrandotex <jose.verdezoto.moran@gmail.com>
2025-08-03 20:39:52 -0500

    Añadido controlador, modelos y configuración de conexión
    - Se creó el controlador `MiControladorController` con acción `Index`.
    - Se añadieron los modelos `Categorias`, `Model1` y `Productos`.
    - Se configuró la relación entre `Categorias` y `Productos`.
    - Se incluyó la carpeta `Views\MiControlador` en el proyecto.
    - Se actualizó `Inventario.csproj` con referencias y nuevos archivos.
    - Se añadió la cadena de conexión `Model1` en `Web.config`.
    - Se corrigió un detalle en `DefaultConnection` en `Web.config`.

---
+++ Inventario/Controllers/MiControladorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventario.Controllers
+{
+    public class MiControladorController : Controller
+    {
+        // GET: MiControlador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Inventario/Models/Productos.cs
@@ -0,0 +1,30 @@
+namespace Inventario.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Productos
+    {
+        public int id { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [StringLength(255)]
+        public string Descripcion { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Valor { get; set; }
+
+        public bool Activo { get; set; }
+
+        public virtual Categorias Categorias { get; set; }
+    }
+}
